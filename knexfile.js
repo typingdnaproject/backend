8d2557e22ba55aeaa779ccb1a8ae4b0da37c94a6
Jamie Goodnight <jamiegoodnight3@gmail.com>
2021-02-10 23:43:08 -0500

    :bug: ssl

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,5 @@
 // Update with your config settings.
 require('dotenv').config();
-var pg = require('pg');
-pg.defaults.ssl = true;
 module.exports = {
 
   development: {
@@ -22,7 +20,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgres',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations'
